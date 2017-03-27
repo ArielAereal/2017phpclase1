2b0d6012ed8a67b8626986b567b051d5185d3321
NebulosaCosmica <ariel257cuevas@yahoo.com.ar>
2017-03-26 23:17:34 -0300

    suma resuelto
    pulido y resuelto. Ver el resto de los ejj

--- clase1/IndexSuma.php
+++ clase1/IndexSuma.php
@@ -1,54 +1,22 @@
 <?php
-   /* $nombre = "Felisa";
+  
+echo "Ejercicio N°1 <br> <br>";            
 
-    echo "Hola Mundo <br>";
+$i;
+$sumados=0;
+$ultimonumero;
+for($i=1;$sumados<1000;$i++)
+{ 
+    $sumados += $i;
 
-    echo $nombre."<br>";
-
-    echo "Hola ".$nombre."<br><br>";
-
-   // var_dump () muestra arrays y objetos
-
-   
-
-   /* while ($suma < 1000)
-   {
-        $suma=$numero+$suma;           
-            $numero = $numero+1;
-            $cuenta = $cuenta+1;      
-
-            if( $suma >1000 || $suma == 1000)
-            {
-                $numero = $numero -1;
-                $suma = $suma - $numero;
-                $cuenta = $cuenta - 1;
-                break;
-            }
-            
-   }*/
-   //intento dos
-   $numeroinicial = 1;
-   $suma = 0;
-   $cuentaynumero = 2;
-
-   $suma = $numeroinicial + $cuentaynumero;
-
-   while ($suma < 1000)
-    {
-    
-    $cuentaynumero = $cuentaynumero + 1;     
-    $suma = $cuentaynumero+$suma;
-
-    if(1000-$suma < 0 )   
-        {
-        $suma = $suma-$cuentaynumero;
-        $cuentaynumero = $cuentaynumero-1;
-        break;
+    if ($sumados >= 1000)
+        { 
+            $sumados -= $i;
+            break; 
         }
-   
-    }
-    $cuentaynumero = $cuentaynumero+2;
-   echo "Cantidad de números utilizados: ".$cuentaynumero."<br>";
-   
-   echo "Resultado de la suma: ".$suma."<br>"; 
+}
+    $ultimonumero = $i-1;
+    echo "Cantidad total de la suma: ".$sumados."<br> <br>";
+    echo "Último número sumado: ".$ultimonumero;
+
 ?>
