5ecf29e6a705527ac6c74e55edcd397cacb5ea00
NebulosaCosmica <ariel257cuevas@yahoo.com.ar>
2017-03-28 16:42:40 -0300

    ejercicios dormidos
    ver de terminarlos

---
+++ clase1/revisar/ejercicio4.php
@@ -0,0 +1,21 @@
+<?php
+    echo "Ejercicio Nº 4";
+    $operador;
+    $op1;
+    $op2;
+    switch($operador)
+    {
+		case "+": ($po1 + $op2)
+	  	break;
+	
+		case "-": ($po1 - $op2)
+	  	break;
+	
+        case "*": ($po1 * $op2)
+	  	break;
+
+        case "/":($po1 / $op2)
+        break;
+	}
+    echo "EL RESULTADO ES:" $operador;
+?>
---
+++ clase1/revisar/ejj3.php
@@ -0,0 +1,47 @@
+<?php
+        echo "Ejercicio Nº 3";
+        $a;
+        $b;
+        $c;
+        $menormayor;
+        $medio;
+        $a=6
+        $b=5
+        $c=7
+        $menormayor=0
+        $medio=0
+    if (a <=b) or (a >=c)
+     {
+        menormayor=a;
+     }
+        else
+        {
+            medio=a;
+        }
+     end if
+      if (b <=a) or (b >=c)
+     {
+        menormayor=b;
+     }
+        else
+        {
+            medio=b;
+        }
+     end if
+      if (c <=a) or (c >=b)
+     {
+        menormayor=c;
+     }
+        else
+        {
+            medio=c;
+        }
+     end if
+      if (medio=0)
+      {echo"NO EXISTE VALOR DEL MEDIO";
+      }
+        else
+        { echo " EL VALOR DEL MEDIO ES: $medio;
+        }
+      end if
+ ?>
