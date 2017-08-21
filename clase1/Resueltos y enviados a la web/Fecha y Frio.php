6bee739069ae5e4f7a88ac6f6dc9effec38ab656
NebulosaCosmica <ariel257cuevas@yahoo.com.ar>
2017-08-21 12:46:23 -0300

    App 2
    casi igual al anterior, retoqué el tema de los hemisferios y las
    estaciones sin tratar de ubicar la localización del hemisferio de la
    zona geográfica establecida en la zona horaria

--- clase1/Resueltos y enviados a la web/Fecha y Frio.php
+++ clase1/Resueltos y enviados a la web/Fecha y Frio.php
@@ -1,8 +1,42 @@
 <?php
 
-echo "<br>Un día, a la hora señalada, la señorita Fecha me preguntó:<br>¿Con qué vestido me veo mejor en esta estación del año?";
+/*Aplicación Nº 2 (Mostrar fecha y estación)
+Obtenga la fecha actual del servidor (función date)
+y luego imprímala dentro de la página con distintos formatos
+(seleccione los formatos que más le guste).
 
-echo "<br><br>Ejercicio Nº 2 <br> <br>";
+Además indicar que estación del año es.
+Utilizar una estructura selectiva múltiple.*/
+
+# manejar la funcion date.
+
+/* https://www.w3schools.com/php/php_date.asp 
+
+timestamp es una marca de tiempo, que todavia no se qué es 
+
+se usa con mktime(hour,etc), devuelve timestamp
+
+date(format,timestamp)
+
+como ejemplo
+
+$d=mktime(11, 14, 54, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
+
+tambien anda con strtotime(...)
+
+caracteres de separación / . -
+
+ver hemisferios
+
+// obtener localización respecto al ecuador. 
+// identificar el hemisferio e indicar precisamente la estación del año
+ 
+*/
+
+echo "<br>Un día, a la hora señalada, la Señorita Fecha me preguntó:
+<br>¿Con qué vestido me veo mejor en esta Estación de trenes?";
+echo "<br><br>Aplicación Nº 2 <br> <br>";
 
 $dia;
 $mes;  
@@ -15,6 +49,8 @@ echo date("d/m/y") ."<br> <br>";
 
 echo date("l jS \of F Y e h:i:s A") ."<br><br> ";
 
+// abajo se zarpa Ariel
+
 $dia = date ("l");
 
 Translate_Dia($dia);
@@ -25,23 +61,23 @@ $mes = date("F");
 
 Translate_Mes($mes);
 
-echo date (" Y ");
+echo " de " . date("Y ");
 
 $zonah = date("e");
 
 if ($zonah == "America/Buenos_Aires")
 {
-    echo "Buenos Aires, Argentina ";
+    echo "Buenos Aires, Argentina.";
 
 } else{ echo date("e");}
 
-echo date (" H:i")."<br> <br>";
+echo " Y son las, a ver..." . date(" H:i")."<br> <br>";
 
 $dia = date ("l");
 
 Translate_Dia($dia);
 
-echo date(" j "). "de ";
+echo date(" j ") . "de ";
 
 $mes = date("F");
 
@@ -52,63 +88,63 @@ echo date (" H:i")."<br><br>";
 switch($mes)
 {
     case "January":
-                    echo ("Estamos en verano.");
+                    echo ("Mientras estemos en el hemisferio sur, será verano.");
                     break;
     case "February":
-                    echo ("Estamos en verano.");
+                    echo ("Mientras estemos en el hemisferio sur, será verano.");
                     break;
     case "March":                   
                     if ($nummes < 21)
                     {
-                   echo ("Estamos en verano.");
+                   echo ("Mientras estemos en el hemisferio sur, será verano.");
                    } else
                    {
-                        echo("Estamos en otoño.");
+                        echo("Mientras estemos en el hemisferio sur, será otoño.");
                    }
                     break;
    case "April":
-                    echo("Estamos en otoño.");
+                    echo("Mientras estemos en el hemisferio sur, será otoño.");
                     break;
    case "May":
-                    echo("Estamos en otoño.");
+                    echo("Mientras estemos en el hemisferio sur, será otoño.");
                     break;
     case "June":
                     if ($nummes < 22)
                     {
-                    echo("Estamos en otoño.");
+                    echo("Mientras estemos en el hemisferio sur, será  otoño.");
                     } else
                     {
-                        echo("Estamos en invierno.");
+                        echo("Mientras estemos en el hemisferio sur, será invierno.");
                     }
                     break;
     case "July":                
-                    echo("Estamos en invierno.");
+                    echo("Mientras estemos en el hemisferio sur, será invierno.");
                     break;
     case "August":                
-                    echo("Estamos en invierno.");
+                    echo("Mientras estemos en el hemisferio sur, será invierno.");
                     break;
     case "September":                
                     if ($nummes < 22)
                     {
-                    echo("Estamos en invierno.");
+                    echo("Mientras estemos en el hemisferio sur, será invierno.");
                     } else
                     {
-                        echo("Estamos en primavera.");
+                        echo("Mientras estemos en el hemisferio sur, será primavera.");
                     }
                     break;
     case "October":                
-                    echo("Estamos en primavera.");
+                    echo("Mientras estemos en el hemisferio sur, será primavera.");
                     break;
     case "November":                
-                    echo("Estamos en primavera.");
+                    echo("Mientras estemos en el hemisferio sur, será primavera.");
                     break;
     case "December":                
                     if ($nummes < 21)
                     {
-                    echo("Estamos en primavera.");
+                    echo("Mientras estemos en el hemisferio sur, será primavera.");
                     } else
                     {
-                        echo("Estamos en verano.");
+                        echo("Mientras estemos en el hemisferio sur, será verano.");
                     }
                     break;
     }
